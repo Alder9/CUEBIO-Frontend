20c5e481d755d26909546e8e684d8d69a2fb7c6e
Alder9 <thomas.alder@colorado.edu>
2020-03-31 19:23:39 -0600

    Added more tile layers, and historic map image overlay which all can be controlled

--- frontend/src/app/apple.service.ts
+++ frontend/src/app/apple.service.ts
@@ -20,7 +20,7 @@ export class AppleService {
   getApples(): Observable<any> {
 
     // return this.http.get(this.baseurl + '/apples/', {headers: this.httpHeaders});
-    return this.http.get('http://127.0.01:3000/beta/query1');
+    return this.http.get('http://127.0.0.1:3000/beta/query1');
    
   }
  
