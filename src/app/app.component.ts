32f13a64be271612e1222e926324c434a59fa301
Akiir3 <erba0445@colorado.edu>
2020-04-03 09:36:26 -0600

    fixed typo:=

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
 
   optionsList: Array<any> = [
     { name: 'Tree Tag ID', attributes: ['Number Input'] },
-    { name: 'Final Cultivar', attributes: ['Ben Davis', 'Fameuse', 'McMahan', 'Delicious', 'Salome', 'McIntosh Summerland Reed', 'Wealthy', 'Irene', 'Haralson', 'Lodi', 'Dolgo', 'Whitney Crab', 'Gimes Golden', ' Winesap', 'Fameuse', 'Duchess/Borowitsky', 'Korei', 'Glenorchie', 'Red Baron', 'Jonathan', 'Arkansas Black', 'Early Strawberry', 'Mann', 'Dorsett Golden', 'Crimson Beauty', 'Empire', 'Hopa', 'Northern Spy', 'Huan Guan', 'King David', 'Imperial Stayman', 'Red Dijmanszoet', 'Secor', 'Haas','Cortland' 'Rome Beauty Law', 'Northwest Greening', 'Yellow Transparent'. 'Tchistaikovka', 'Earliblaze', 'Pepin Litovskit'] },
+    { name: 'Final Cultivar', attributes: ['Ben Davis', 'Fameuse', 'McMahan', 'Delicious', 'Salome', 'McIntosh Summerland Reed', 'Wealthy', 'Irene', 'Haralson', 'Lodi', 'Dolgo', 'Whitney Crab', 'Gimes Golden', ' Winesap', 'Fameuse', 'Duchess/Borowitsky', 'Korei', 'Glenorchie', 'Red Baron', 'Jonathan', 'Arkansas Black', 'Early Strawberry', 'Mann', 'Dorsett Golden', 'Crimson Beauty', 'Empire', 'Hopa', 'Northern Spy', 'Huan Guan', 'King David', 'Imperial Stayman', 'Red Dijmanszoet', 'Secor', 'Haas','Cortland', 'Rome Beauty Law', 'Northwest Greening', 'Yellow Transparent', 'Tchistaikovka', 'Earliblaze', 'Pepin Litovskit'] },
     { name: 'Country', attributes: ['United States', 'Canada', 'Former Soviet Union', 'United Kingdom', 'Japan', 'Bahamas', 'Netherlands/UK', 'Russia', 'China'] },
     { name: 'Tree Height', attributes: ['Number Input'] },
     { name: 'Trip Dripline', attributes: ['Number Input'] },
